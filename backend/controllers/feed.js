bfef104c727a110dfbfc6b3e88f36ca4337bd5ed
Mariia Ptytsyna <ptitsynamv@gmail.com>
2024-04-18 09:04:38 +0300

    Add async and await

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -12,22 +12,21 @@ function getItemsForPage(items, page) {
   return items.slice(skip, skip + limit);
 }
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   const page = req.query.page || 1;
 
-  Post.fetchAll()
-    .then((posts) => {
-      return res.status(200).json({
-        posts: getItemsForPage(posts, page),
-        totalItems: posts.length,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const posts = await Post.fetchAll();
+    res.status(200).json({
+      posts: getItemsForPage(posts, page),
+      totalItems: posts.length,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.createPost = (req, res, next) => {
